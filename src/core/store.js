09f6234924643b53873e984cd5a34d95125655a1
BlueDragon33 <dinhnam331007@gmail.com>
2026-09-06 19:38:24 +0700

    refactor(data): use schema v4 blank state

--- src/core/store.js
+++ src/core/store.js
@@ -1,10 +1,10 @@
-import { CURRENT_SCHEMA_VERSION, blankStateV3, migrateState } from './schema.js';
+import { CURRENT_SCHEMA_VERSION, blankStateV4, migrateState } from './schema.js';
 
 const STORAGE_KEY = 'growup_mychildren_v1';
 const MAX_AUDIT_ITEMS = 200;
 
 export function blankState() {
-  return blankStateV3();
+  return blankStateV4();
 }
 
 export function loadState() {
