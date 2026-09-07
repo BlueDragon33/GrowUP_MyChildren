d0476bee843d47fc404ed23f370fc8f223783cf8
BlueDragon33 <dinhnam331007@gmail.com>
2026-09-07 08:59:54 +0700

    refactor(v10): consolidate app runtime entrypoint

---
+++ src/runtime-entry.js
@@ -0,0 +1,11 @@
+import './app.js';
+import './enhancements.js';
+import './v4.js';
+import './v5.js';
+import './v6.js';
+import './v7.js';
+import './v8.js';
+import './v9-runtime.js';
+import './v9-compat.js';
+import './v10-runtime.js';
+import './a11y.js';
